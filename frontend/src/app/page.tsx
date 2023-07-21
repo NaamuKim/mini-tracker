d6184996babcaf97028a20be91fc86b598c6c261
Jiwon Kim <lost731@naver.com>
2023-07-21 18:00:17 +0900

    feat(front): Link 통한 라우팅 방식으로 변경

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
-import styles from './page.module.css'
+import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        Main Landing Page
-      </div>
+      <div className={styles.description}>Main Landing Page</div>
+      <Link href="/second">
+        <h2>Go to second page</h2>
+      </Link>
     </main>
-  )
+  );
 }
--- frontend/src/app/second/page.tsx
+++ frontend/src/app/second/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-    return (
-        <main>
-            <div>
-                Second Landing Page
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <div>Second Landing Page</div>
+      <Link href="/">
+        <h2>Go to first page</h2>
+      </Link>
+    </main>
+  );
 }
