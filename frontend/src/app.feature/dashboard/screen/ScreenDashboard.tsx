39c88ec82d44324f2d08709d16d486d24eb0cd6b
Jiwon Kim <lost731@naver.com>
2023-08-02 20:37:21 +0900

    feat(front): page-transition sankey chart publishing

--- frontend/src/app.feature/dashboard/screen/ScreenDashboard.tsx
+++ frontend/src/app.feature/dashboard/screen/ScreenDashboard.tsx
@@ -2,11 +2,13 @@
 import React from "react";
 import styled from "styled-components";
 import TabBar from "@/app.feature/dashboard/component/client/TabBar";
+import Dashboards from "@/app.feature/dashboard/component/client/Dashboards";
 const ScreenDashboard = () => {
   return (
     <StyledWrapper>
       <h2>Mini Dashboard</h2>
       <TabBar />
+      <Dashboards />
     </StyledWrapper>
   );
 };
