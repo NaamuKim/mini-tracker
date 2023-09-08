fbc1699e57a687d463781cf69081f6a98ae70f89
Jiwon Kim <lost731@naver.com>
2023-09-08 20:44:03 +0900

    feat(page-views): vertical bar chart that represents top pages that user views

--- frontend/src/app.feature/dashboard/component/client/Dashboards.tsx
+++ frontend/src/app.feature/dashboard/component/client/Dashboards.tsx
@@ -4,17 +4,37 @@ import BarPageTransition from "@/app.feature/dashboard/component/client/chart/Ba
 import SankeyPageTransition from "@/app.feature/dashboard/component/client/chart/SankeyPageTransition";
 import GoingPageView from "@/app.feature/dashboard/page-view/component/chart/GoingPageView";
 
+import SimpleBarChart from "@/app.components/dashboard/SimpleBarChart";
+import DashboardBackground from "@/app.feature/dashboard/component/client/DashboardBackground";
+
 const Dashboards = () => {
   return (
     <StyledWrapper>
-      <div className="quad-section">
-        <GoingPageView />
-        <div>hi</div>
-        <div>hi</div>
-        <div>hi</div>
+      <div>
+        <div className="quad-section">
+          <GoingPageView />
+          <GoingPageView />
+          <GoingPageView />
+          <GoingPageView />
+        </div>
+        <BarPageTransition />
+        <SankeyPageTransition />
       </div>
-      <BarPageTransition />
-      <SankeyPageTransition />
+      <DashboardBackground title="Top Pages">
+        <SimpleBarChart
+          data={[
+            { name: "Page A", view: 240 },
+            { name: "Page B", view: 2210 },
+            { name: "Page C", view: 2300 },
+            { name: "Page D", view: 2000 },
+            { name: "Page E", view: 0 },
+            { name: "Page F", view: 123 },
+            { name: "Page G", view: 2091 },
+          ]}
+          xKey="name"
+          yKey="view"
+        />
+      </DashboardBackground>
     </StyledWrapper>
   );
 };
@@ -23,10 +43,20 @@ export default Dashboards;
 
 const StyledWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   gap: 30px;
   margin-top: 30px;
   .quad-section {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
+  }
+  > div {
+    display: flex;
+    gap: 30px;
+  }
+  .quad-section {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 30px;
   }
 `;
