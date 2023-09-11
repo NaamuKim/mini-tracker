dbd27d90f45f82449aa66ed48634ac7c753a616b
Jiwon Kim <lost731@naver.com>
2023-09-11 22:37:41 +0900

    feat(set-prisma): set prisma scheme and configuration

--- backend/src/middlewares/setup.ts
+++ backend/src/middlewares/setup.ts
@@ -2,17 +2,20 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { pipeMiddlewares } from "../fns/pipeMiddlewares";
-import { connectionMiddleware } from "./database";
+import { errorHandler } from "./errors/database";
 
 export const setupMiddleware = (app: express.Application) => {
-  pipeMiddlewares(app, [
-    express.json(),
-    cors({
-      origin: "http://localhost:3000",
-      credentials: true,
-    }),
-    cookieParser(),
-    express.urlencoded({ extended: true }),
-    connectionMiddleware,
-  ]);
+  pipeMiddlewares(
+    app,
+    [
+      express.json(),
+      cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+      }),
+      cookieParser(),
+      express.urlencoded({ extended: true }),
+    ],
+    [errorHandler],
+  );
 };
