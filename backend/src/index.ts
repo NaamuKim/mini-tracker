c9bf525a32991dbf61f05db79b95036735bfc99d
Jiwon Kim <lost731@naver.com>
2023-09-15 21:14:23 +0900

    chore: backend prisma disconnect when server close

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,22 @@
 import app from "@/app";
+import prisma from "@/config/db";
 
 const port = 8080;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`mini-tracker-api-server listening on port ${port}`);
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT received. Closing server...");
+  server.close(async (err) => {
+    if (err) {
+      console.error("Failed to close server", err);
+      process.exit(1);
+    } else {
+      await prisma.$disconnect();
+      console.log("Server closed.");
+      process.exit(0);
+    }
+  });
 });
