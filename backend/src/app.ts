7b0b411f1c002fc8ad1bff00d40b10e6542719d5
Jiwon Kim <lost731@naver.com>
2023-09-15 19:10:31 +0900

    chore: delete legacy code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { setupMiddleware } from "@/middlewares/setup";
 import routes from "@/routes";
-import userEventRoutes from "@/routes/event";
 import pageViewRoutes from "@/routes/pageView";
 
 const app = express();
@@ -9,7 +8,6 @@ const app = express();
 setupMiddleware(app);
 
 app.use("/", routes);
-app.use("/event", userEventRoutes);
 app.use("/page-view", pageViewRoutes);
 
 export default app;
