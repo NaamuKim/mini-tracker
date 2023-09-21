898619ef753679631bae9ea710a2afed73419523
Jiwon Kim <lost731@naver.com>
2023-09-21 18:55:09 +0900

    refactor(sdk-pv-tagging): make ioc register key to constant

--- sdk/src/app.ts
+++ sdk/src/app.ts
@@ -1,6 +1,8 @@
 import IOCContainer from "@/core/container/IOCContatiner";
 import SDKInitializer from "@/core/SDKInitializer";
 import AbstractStorage from "@/core/storage";
+import { STORAGE_KEYS } from "@/constants/storage";
+import { IOC_DEPENDENCIES_KEYS } from "@/constants/ioc";
 
 const main = () => {
   const availableStorage = AbstractStorage.getAvailableStorage();
@@ -8,7 +10,10 @@ const main = () => {
     throw new Error("No available storage");
   }
 
-  IOCContainer.getInstance().register("Storage", availableStorage);
+  IOCContainer.getInstance().register(
+    IOC_DEPENDENCIES_KEYS.STORAGE,
+    availableStorage,
+  );
 
   new SDKInitializer();
 };
