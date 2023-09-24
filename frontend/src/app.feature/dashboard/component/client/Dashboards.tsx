74faf76c2e29128d75fa47677f26bd9230a46e18
Jiwon Kim <lost731@naver.com>
2023-09-24 21:58:24 +0900

    feat(daily-visitor-graph): add daily visitor graph markup

--- frontend/src/app.feature/dashboard/component/client/Dashboards.tsx
+++ frontend/src/app.feature/dashboard/component/client/Dashboards.tsx
@@ -1,43 +1,50 @@
-import React from "react";
-import styled from "styled-components";
-import BarPageTransition from "@/app.feature/dashboard/component/client/chart/BarPageTransition";
-import SankeyPageTransition from "@/app.feature/dashboard/component/client/chart/SankeyPageTransition";
-import GoingPageView from "@/app.feature/dashboard/page-view/component/chart/GoingPageView";
+import React from 'react';
+import styled from 'styled-components';
+import { Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
-import SimpleBarChart from "@/app.components/dashboard/SimpleBarChart";
-import DashboardBackground from "@/app.feature/dashboard/component/client/DashboardBackground";
-
-const Dashboards = () => {
-  return (
-    <StyledWrapper>
-      <div>
-        <div className="quad-section">
-          <GoingPageView />
-          <GoingPageView />
-          <GoingPageView />
-          <GoingPageView />
-        </div>
-        <BarPageTransition />
-        <SankeyPageTransition />
-      </div>
-      <DashboardBackground title="Top Pages">
-        <SimpleBarChart
-          data={[
-            { name: "Page A", view: 240 },
-            { name: "Page B", view: 2210 },
-            { name: "Page C", view: 2300 },
-            { name: "Page D", view: 2000 },
-            { name: "Page E", view: 0 },
-            { name: "Page F", view: 123 },
-            { name: "Page G", view: 2091 },
-          ]}
-          xKey="name"
-          yKey="view"
-        />
-      </DashboardBackground>
-    </StyledWrapper>
-  );
-};
+const Dashboards = () => (
+  <StyledWrapper>
+    <ResponsiveContainer width='100%' height={300}>
+      <LineChart
+        data={[
+          { name: '09.20', view: 2000 },
+          {
+            name: '09.21',
+            view: 2300,
+          },
+          { name: '09.22', view: 1200 },
+          { name: '09.23', view: 1400 },
+          { name: '09.24', view: 3200 },
+          { name: '09.25', view: 2500 },
+          { name: '09.26', view: 1300 },
+          { name: '09.27', view: 3200 },
+        ]}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <XAxis dataKey='name' />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type='monotone' dataKey='view' stroke='var(--link-color)' />
+      </LineChart>
+    </ResponsiveContainer>
+    {/* <DashboardBackground title="Top Pages">
+          <SimpleBarChart
+            data={[
+              { name: "Page A", view: 240 },
+              { name: "Page B", view: 2210 },
+              { name: "Page C", view: 2300 },
+              { name: "Page D", view: 2000 },
+              { name: "Page E", view: 0 },
+              { name: "Page F", view: 123 },
+              { name: "Page G", view: 2091 },
+            ]}
+            xKey="name"
+            yKey="view"
+          />
+        </DashboardBackground> */}
+  </StyledWrapper>
+);
 
 export default Dashboards;
 
