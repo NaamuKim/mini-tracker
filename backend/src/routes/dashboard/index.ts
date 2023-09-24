1829c5e4a1475ad03b314a93b1059ef0c115831e
Jiwon Kim <lost731@naver.com>
2023-09-24 11:44:19 +0900

    refactor(BE-routes): reorganize routes

---
+++ backend/src/routes/dashboard/index.ts
@@ -0,0 +1,7 @@
+import loginRouter from "@/routes/dashboard/login";
+import overviewRouter from "@/routes/dashboard/overview";
+
+export const dashboardRouter = {
+  loginRouter,
+  overviewRouter,
+};
