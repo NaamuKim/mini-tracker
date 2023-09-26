5a1ee33acca61f8fa0e1e85ab35ee006135f3f4d
Jiwon Kim <lost731@naver.com>
2023-09-26 20:15:44 +0900

    fix: add express index.d.ts import express-serve-static-core

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -1,4 +1,6 @@
-declare module 'express-serve-static-core' {
+import "express-serve-static-core";
+
+declare module "express-serve-static-core" {
   export interface Request<T = unknown> {
     validatedQueryParams: T;
   }
