86b0dcc2d351b3020f064d2ec61a9fa06d2bc20c
Jiwon Kim <lost731@naver.com>
2023-09-26 21:23:18 +0900

    fix: explicit casting at visitors dashboard api

--- backend/src/routes/dashboard/overview.ts
+++ backend/src/routes/dashboard/overview.ts
@@ -1,24 +1,32 @@
-import express from 'express';
-import { retrieveVisitors } from '@/services/dashboard/overview';
-import { VisitorsQueryParamsDTO } from '@/types/DTO/dashboard/overview';
-import { validateVisitorsQueryParam } from '@/middlewares/validators/dashboard/overview';
-import { Request } from 'express-serve-static-core';
+import express from "express";
+import { retrieveVisitors } from "@/services/dashboard/overview";
+import { VisitorsQueryParamsDTO } from "@/types/DTO/dashboard/overview";
+import { validateVisitorsQueryParam } from "@/middlewares/validators/dashboard/overview";
 
 const router = express.Router();
 
-router.get('/visitors', validateVisitorsQueryParam, async (req: Request<VisitorsQueryParamsDTO>, res) => {
-  const { startDate, endDate, interval = 'day', sort = 'asc', limit = 50 } = req.validatedQueryParams;
+router.get("/visitors", validateVisitorsQueryParam, async (req, res) => {
+  const {
+    startDate,
+    endDate,
+    interval = "day",
+    sort = "asc",
+    limit = 50,
+  } = req.validatedQueryParams as VisitorsQueryParamsDTO;
 
   try {
     const parsedStartDate = new Date(startDate);
     const parsedEndDate = new Date(endDate);
 
-    if (Number.isNaN(parsedStartDate.getTime()) || Number.isNaN(parsedEndDate.getTime())) {
-      return res.status(400).json({ message: 'Invalid date' });
+    if (
+      Number.isNaN(parsedStartDate.getTime()) ||
+      Number.isNaN(parsedEndDate.getTime())
+    ) {
+      return res.status(400).json({ message: "Invalid date" });
     }
 
-    if (interval !== 'day' && interval !== 'month') {
-      return res.status(400).json({ message: 'Invalid interval' });
+    if (interval !== "day" && interval !== "month") {
+      return res.status(400).json({ message: "Invalid interval" });
     }
 
     const validatedVisitors: VisitorsQueryParamsDTO = {
@@ -32,7 +40,7 @@ router.get('/visitors', validateVisitorsQueryParam, async (req: Request<Visitors
     const visitors = await retrieveVisitors(validatedVisitors);
 
     res.status(200).json({
-      message: 'Visitors retrieved',
+      message: "Visitors retrieved",
       success: true,
       data: {
         visitors,
@@ -41,7 +49,7 @@ router.get('/visitors', validateVisitorsQueryParam, async (req: Request<Visitors
   } catch (err) {
     console.log(err);
     res.status(500).json({
-      message: 'Internal server error',
+      message: "Internal server error",
     });
   }
 });
