487914e4e50b64aac0d794f3a17533b2e4f58cf6
Jiwon Kim <lost731@naver.com>
2023-09-27 11:38:20 +0900

    feat(next-config-js): set alias

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const path = require("path");
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      "@": path.resolve(__dirname, "src/"),
+    };
+    return config;
+  },
+};
 
 module.exports = nextConfig;
