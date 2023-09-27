7c40e7ccb9abbca9f8b32a7eab426a0d317f2086
Jiwon Kim <lost731@naver.com>
2023-09-27 16:38:03 +0900

    refactor(fe-api-routes): change api route directory structure

--- frontend/src/app.module/constant/api/app.dashboard.ts
+++
@@ -1,3 +0,0 @@
-export const API_SANKEY_TRANSITION = '/event/page-transitions';
-export const API_BEST_BARS_TRANSITION = '/event/page-transition-best';
-export const API_DAILY_VIEW = '/overview/visitors';
