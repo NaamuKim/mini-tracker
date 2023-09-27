007fb776bd45ae932b388b76bec65e351d9de357
Jiwon Kim <lost731@naver.com>
2023-09-27 12:18:31 +0900

    refactor: change useQueryFn to get response.data

--- frontend/src/app.module/react-query/useQueryFn.ts
+++ frontend/src/app.module/react-query/useQueryFn.ts
@@ -1,27 +1,54 @@
-import { useQuery, UseQueryOptions } from 'react-query';
-import { fetcher } from '@/app.module/fetcher';
+import { useQuery, UseQueryOptions } from "react-query";
+import { fetcher } from "@/app.module/fetcher";
 
-const objectToQueryParamString = (obj?: Record<string, string | number | boolean>) => {
+const objectToQueryParamString = (
+  obj?: Record<string, string | number | boolean>,
+) => {
   if (!obj) return null;
   const params = new URLSearchParams();
   for (const [key, value] of Object.entries(obj)) {
     params.append(key, String(value));
   }
-  return '?' + params.toString();
+  return "?" + params.toString();
+};
+
+const requestFn = async ({
+  url,
+  method,
+  body,
+}: {
+  url: string;
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  body?: any;
+}) => {
+  try {
+    const response: {
+      data: any;
+      message: string;
+    } = await fetcher({ url, method, body });
+    return response?.data;
+  } catch (err) {
+    console.error(err);
+    return Promise.reject(err);
+  }
 };
 
 export const useQueryFn = <ResponseData, SelectedData = ResponseData>(
   queryKeys: [string, ...Record<string, any>[]],
   options: UseQueryOptions<ResponseData, unknown, SelectedData> & {
     customQueryKeys?: any;
-  } = {}
+  } = {},
 ) =>
   useQuery<ResponseData, unknown, SelectedData>(
     queryKeys,
-    () => fetcher({ url: queryKeys[0] + objectToQueryParamString(queryKeys[1]), method: 'GET' }),
+    async () =>
+      await requestFn({
+        url: queryKeys[0] + objectToQueryParamString(queryKeys[1]),
+        method: "GET",
+      }),
     {
       ...options,
-    }
+    },
   );
 
 export default useQueryFn;
