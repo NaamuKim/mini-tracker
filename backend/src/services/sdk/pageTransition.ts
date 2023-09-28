86a3debce1ad8c917178e9fc6c048f088c7cf741
Jiwon Kim <lost731@naver.com>
2023-09-28 18:02:01 +0900

    feat(api): page view api get fromPageExitTIme  (#16)

--- backend/src/services/sdk/pageTransition.ts
+++ backend/src/services/sdk/pageTransition.ts
@@ -1,14 +1,25 @@
 import { PageView } from "@prisma/client";
 import { insertPageTransition } from "@/models/sdk/pageTransition";
-import { findRecentPageViewBySession } from "@/models/sdk/pageView";
+import {
+  findRecentPageViewBySession,
+  insertPageExitTime,
+} from "@/models/sdk/pageView";
 import { PageTransitionCreateInputWithoutFromTo } from "@/types/pageTransition";
 
 export const registerPageTransition = async (
   sessionId: PageView["sessionId"],
   toPageViewInfo: PageView,
   pageTransitionInfo: PageTransitionCreateInputWithoutFromTo,
+  fromPageExitTime?: Date,
 ) => {
   const fromPageViewId = await findRecentPageViewBySession(sessionId);
+  if (fromPageExitTime) {
+    await insertPageExitTime({
+      fromPageViewId,
+      exitTime: fromPageExitTime,
+    });
+  }
+
   const pageTransition = await insertPageTransition({
     pageTransitionInfo,
     fromPageViewId,
