008b2dd1d537cf318ddf68e67ea32c05811e64ed
Jiwon Kim <lost731@naver.com>
2023-10-23 20:06:12 +0900

    feat(daily-view-api): Modify SQL query to always retrieve 7 distinct dates
    - Refactored JOIN conditions and WHERE filters for improved accuracy and clarity.
    - Ensured that the query consistently retrieves data for 7 unique dates, regardless of data gaps.

--- backend/src/models/dashboard/overview/visitors.ts
+++ backend/src/models/dashboard/overview/visitors.ts
@@ -3,32 +3,43 @@ import { Prisma } from "@prisma/client";
 import prisma from "@/config/db";
 
 export const findVisitors = async ({
-  startDate,
   endDate,
   sort,
   queriedUrl,
 }: VisitorsQueryParamsDTO): Promise<
   Array<{
-    entryTime: Date;
+    date: Date;
     count: number;
   }>
 > => {
   const sortingClause =
     sort === "asc"
-      ? Prisma.sql`ORDER BY Date(entryTime) ASC`
-      : Prisma.sql`ORDER BY Date(entryTime) DESC`;
+      ? Prisma.sql`ORDER BY RankedDates.entryTime ASC`
+      : Prisma.sql`ORDER BY RankedDates.entryTime DESC`;
 
   const visitors = await prisma.$queryRaw<
     Array<{
-      entryTime: Date;
+      date: Date;
       count: bigint;
     }>
   >`
-    SELECT DATE(entryTime) as date, COUNT(*) as count 
-    FROM PageView 
-    WHERE entryTime BETWEEN ${startDate} AND ${endDate} AND baseUrl = ${queriedUrl}
-    GROUP BY DATE(entryTime) 
-    ${sortingClause}
+      WITH RankedDates AS (
+          SELECT
+              DATE(entryTime) as entryTime,
+              RANK() OVER (ORDER BY DATE(entryTime) DESC) as rnk
+          FROM PageView
+          WHERE entryTime BETWEEN ${"2023-09-01"} AND ${endDate} AND baseUrl = ${queriedUrl}
+          GROUP BY DATE(entryTime)
+      )
+
+      SELECT
+          RankedDates.entryTime as date,
+          COALESCE(COUNT(PageView.entryTime), 0) as count
+      FROM RankedDates
+               LEFT JOIN PageView ON DATE(PageView.entryTime) = RankedDates.entryTime AND baseUrl = ${queriedUrl}
+      WHERE RankedDates.rnk <= 7
+      GROUP BY RankedDates.entryTime
+          ${sortingClause}
   `;
 
   return visitors.map((visitor) => ({
